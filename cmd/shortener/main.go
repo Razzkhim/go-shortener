7a07ff4dff5a80d1743849633a4e703997983763
rakhim.marat <r>
2024-04-16 16:27:36 +0500

    http serve

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+type Handler struct{}
+
+func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello!")
+	res.Write(data)
+}
 
 func main() {
-	fmt.Println(123)
-	fmt.Println("Hello")
+	var h Handler
+
+	err := http.ListenAndServe(`:8080`, h)
+	if err != nil {
+		panic(err)
+	}
+
 }
