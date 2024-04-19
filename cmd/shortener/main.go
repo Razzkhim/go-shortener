5de8429a17cefb0e68e6db06256a734b92007e97
rakhim.marat <r>
2024-04-19 10:29:04 +0500

    json

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,26 +1,57 @@
 package main
 
-import "net/http"
-
-//type Handler struct{}
-//
-//func (Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-//	data := []byte("hi")
-//	res.Write(data)
-//}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func mainPage(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Hello"))
+	body := fmt.Sprintf("Method: %s\r\n", req.Method)
+	body += "Header ===============\r\n"
+	for k, v := range req.Header {
+		body += fmt.Sprintf("%s: %v\r\n", k, v)
+	}
+	body += "Query parameters ===============\r\n"
+
+	if err := req.ParseForm(); err != nil {
+		res.Write([]byte(err.Error()))
+		return
+	}
+
+	for k, v := range req.Form {
+		body += fmt.Sprintf("%s: %v\r\n", k, v)
+	}
+	res.Write([]byte(body))
 }
 
-func apiPage(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("APIIIIIIIIIII"))
+type Subj struct {
+	Product string `json:"name"`
+	Price   int    `json:"price"`
+}
+
+func JSONHandler(w http.ResponseWriter, req *http.Request) {
+	// собираем данные
+	subj := Subj{"Milk", 50}
+	// кодируем в JSON
+	resp, err := json.Marshal(subj)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	// устанавливаем заголовок Content-Type
+	// для передачи клиенту информации, кодированной в JSON
+	w.Header().Set("content-type", "application/json")
+	// устанавливаем код 200
+	w.WriteHeader(http.StatusOK)
+	// пишем тело ответа
+	w.Write(resp)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/asdsa`, apiPage)
 	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/hi`, JSONHandler)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
